ea7a243407f7440224186993c213059481a37ec4
RDB <rdbwebdeveloper@gmail.com>
2020-09-06 19:39:10 +0800

    update
    updated navigation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <template>
     <div id="app">
-        <div id="nav" class="py-5">
-            <div class="container">
-                <router-link to="/">Home</router-link> |
-                <router-link to="/about">About</router-link>
+        <div id="nav" class="flex p-5 border-b-2">
+            <div class="logo w-1/4">
+                <img class="w-1/2" src="./assets/images/logo.png" />
+            </div>
+
+            <div class="right-nav w-3/4 flex justify-end items-center">
+                <div class="grid grid-cols-3 gap-4">
+                    <div class="col-span-2">
+                        <router-link class="m-3" to="/">Home</router-link>
+                        <router-link class="m-3" to="/about">About</router-link>
+                        <router-link class="m-3" to="/about">Shop</router-link>
+                        <router-link class="m-3" to="/about">Contact</router-link>
+                    </div>
+
+                    <div class="col-span-1">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-5">
+                            Login
+                        </button>
+
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Register
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
         <router-view/>
