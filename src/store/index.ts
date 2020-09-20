1b63d0467f756b6aed3c7145f63ae014b908ace9
Reece <trazhme@gmail.com>
2020-09-20 17:04:01 +0800

    update
    updated store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import product from './modules/product'
 
 export default createStore({
   state: {
@@ -8,5 +9,6 @@ export default createStore({
   actions: {
   },
   modules: {
+    product
   }
 })
